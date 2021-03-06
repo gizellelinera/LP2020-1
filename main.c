03e234642d68ec070387568a059c8406e6390a6f
Gizelle Lima <gizellelinera3@gmail.com>
2021-03-06 18:53:36 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void) {
+  float a,b,c, maior;
+	scanf("%f %f %f",&a, &b,&c);
+	
+	if(a>b){
+		maior=a;
+	}else{
+		  maior=b;
+	}
+	if(c>maior){
+	  	maior=c;
+	}
+	if((a+b+c-maior)<= maior){
+	    printf("nao forma\n");
+	  }else if (a==b && b==c){
+	    printf("equilatero\n"); 
+	  }else if(a==b || a==c|| b==c){
+		  printf(" isosceles\n");
+	  }else{
+	    printf(" escaleno\n");
+	  }
+	
+	return 0;
+}
+
+
