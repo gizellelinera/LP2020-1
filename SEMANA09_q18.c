739cc627d9acdc7882ebba13c514641b86567b4f
Gizelle Lima <>
2021-05-15 15:53:35 +0000

    SEMANA9

--- SEMANA09_q18.c
+++ SEMANA09_q18.c
@@ -1,15 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main() {
-  	int vetor[99], i=0,num;
+// #include <stdio.h>
+// #include <stdlib.h>
+// int main() {
+//   	int vetor[99], i=0,num;
 	
-		scanf("%i",&num);
-	for(i=0;i<num;i++){
-		scanf("%i",&vetor[i]);  //ATRIBUI VALOR AS POSIÇÕES DO VETOR
-	}
-	for(i=num-1;i>=0;i--){     // INVERTE A POSIÇÃO DO VETOR
-	//	printf("%i",i);
-		printf("%i",vetor[i]);  //IMPRIMIR O RESULTADO FINAL
-	}
-  return 0;
-}
+// 		scanf("%i",&num);
+// 	for(i=0;i<num;i++){
+// 		scanf("%i",&vetor[i]);  //ATRIBUI VALOR AS POSIÇÕES DO VETOR
+// 	}
+// 	for(i=num-1;i>=0;i--){     // INVERTE A POSIÇÃO DO VETOR
+// 	//	printf("%i",i);
+// 		printf("%i",vetor[i]);  //IMPRIMIR O RESULTADO FINAL
+// 	}
+//   return 0;
+// }
---
+++ SEMANA09_q19.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+ #include <stdlib.h>
+ int main() {
+   int tam, l,c,i=0;
+	int matriz[99][99];
+	char C;
+	
+	scanf("%i",&tam);
+	scanf("%s",&C);
+
+	for(l=0;l<tam;l++){
+		for(c=0;c<tam;c++){
+		
+			// FAZER MATRIZ DIAGONAL
+			if(l==c && C=='D')
+					printf("1");
+				else{
+					if(C=='D'){
+						printf("0");
+					}
+				}
+			
+			// FAZER A TRIANGULAR SUPERIOR
+			
+				if(l<c && C=='S'){
+					printf("1");
+				}
+				if(l==c && C=='S'){
+					printf("1");
+				}
+				if(l>c && C=='S'){
+					printf("0");
+				}
+					
+			// FAZER A TRIANGULAR INFERIOR
+				
+				else{
+					if(l<c && C=='I'){
+						printf("0");
+					}
+					if(l==c && C=='I'){
+						printf("1");
+					}
+					if(l>c && C=='I'){
+						printf("1");
+					}
+				}
+			//FAZER MATRIZ TABULEIRO
+				
+				if ((l+c)%2==0  && C=='T'){
+					printf("1");
+				}
+				else{
+					if((l+c)%2==1 && C=='T'){
+						printf("0");
+					}
+				}
+		}
+		printf("\n");
+	}
+return 0;
+}
+		
+			
